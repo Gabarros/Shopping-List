77c61166ee2224c7100a77581980cf2a01d9b4b8
Gabarros <gabarros17@gmail.com>
2019-12-02 23:54:33 -0300

    Create shopping list component

---
+++ client-frontend/src/components/ShoppingList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component{
+    state = {
+        items: [
+            { id: uuid(), name: 'Eggs'},
+            { id: uuid(), name: 'Milks'},
+            { id: uuid(), name: 'Steack'},
+            { id: uuid(), name: 'Meat'}
+        ]
+    }
+
+    render(){
+        const{ items } = this.state;
+
+        return(
+            <Container>
+                <Button color="dark"
+                style={{marginBottom: '2rem'}}
+                onClick={()=>{
+                    const name = prompt('enter item');
+                    if(name){
+                        this.setState(state => ({
+                            items: [...state.items, { id: uuid(), name}]
+                        }));
+                    }
+                }}
+                >Add Item</Button>
+            </Container>
+        )
+    }
+
+    
+
+
+}
+
+export default ShoppingList;
